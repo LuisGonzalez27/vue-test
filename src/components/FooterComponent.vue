df1760582c9d892257e4d6dd0457e0c769742932
LuisGonzalez27 <luis27coding@gmail.com>
2022-11-09 12:40:46 +0100

    test con Vite

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer>
+        {{ titolo }}
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterComponent',
+    data() {
+        return {
+            titolo: 'Footer, sono un componente'
+        }
+    }
+}
+</script>
+
+<style lang="css">
+footer {
+    width: 100%;
+    height: 70px;
+    background-color: yellow;
+    padding: 20px;
+}
+</style>
---
+++ src/components/HeaderComponet.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        {{ titolo }}
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+    data() {
+        return {
+            titolo: 'Header ,sono un componente!'
+        }
+    }
+}
+</script>
+
+<style lang="css">
+header {
+    width: 100%;
+    height: 70px;
+    background-color: black;
+    padding: 20px;
+    color: white;
+}
+</style>
